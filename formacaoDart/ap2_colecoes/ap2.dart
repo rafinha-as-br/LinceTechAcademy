f66e81e68c73073a9a2134a6237a44ffaf38c867
Rafael Antunes <rafinha72465@gmail.com>
2025-04-01 23:41:07 -0300

    Atividade: ap2_colecoes, ap2.dart concluída!

---
+++ formacaoDart/ap2_colecoes/ap2.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+
+void main(){
+  //cria a lista dinâmica
+  List<int> lista = [];
+
+  //preenche essa lista e já salva na variável de controle para impressão
+  String listaOriginal = "";
+  for(int i=0; i<10; i++){
+    lista.add(Random().nextInt(100));
+    listaOriginal += " ${lista[i].toString()};";
+  }
+
+  //imprime a lista original usando a variável usada durante o loop de preenchimento
+  print("Lista original: ");
+  print("$listaOriginal");
+
+  //realiza a remoção dos números pares
+  lista.removeWhere((item)=> item%2 == 0);
+
+  //imprime a lista atualizada
+  String novaLista = " ";
+  print("Lista atualizada: ");
+  lista.forEach((item) => novaLista += " ${item.toString()};");
+  print(novaLista);
+}
