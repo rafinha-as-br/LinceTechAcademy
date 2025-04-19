ac796116cf421a18a9b35f4b5e22ff6294f83cdd
Rafael Antunes <rafinha72465@gmail.com>
2025-04-19 20:15:58 -0300

    projeto base concluído

--- desafios/clima/bin/clima.dart
+++ desafios/clima/bin/clima.dart
@@ -1,6 +1,19 @@
-import 'package:clima/clima.dart' as clima;
+import 'dart:io';
+import 'gerarRelatorio.dart';
 
 
-void main() {
+void main() async{
+  print("Olá Leandro! Que relatório você precisa?");
+  print("1 - Temperatura");
+  print("2 - Umidade");
+  print("3 - Direção do vento");
+  print("4 - Para sair");
 
+  int opcao = int.tryParse(stdin.readLineSync()?? "")?? 0;
+  final dir = Directory('arquivosCSV');
+  try{
+    await criarRelatorio(opcao, dir);
+  } catch (e){
+    print("erro ao gerar relatório");
+  }
 }
--- desafios/clima/bin/leitorCSV.dart
+++ desafios/clima/bin/leitorCSV.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:csv/csv.dart';
 import 'package:path/path.dart';
 
-// classe que registra os dados de cada linha
 class Dados {
   final String estado;
   final DateTime dataHora;
@@ -12,57 +11,61 @@ class Dados {
   final double velVento;
   final double dirVento;
 
-  Dados({required this.estado, required this.dataHora, required this.temperatura, required this.umidade, required this.velVento, required this.dirVento});
-
+  Dados({required this.estado, required this.dataHora, required this.temperatura,
+    required this.umidade, required this.velVento, required this.dirVento});
 }
 
-//função lerArquivos
-Future<List<Dados >> lerArquivos(Directory dir) async{
-  //conferir se o diretório existe
-  if(!await dir.exists()){
+Future<List<Dados>> lerArquivos(Directory dir) async {
+  if(!await dir.exists()) {
     throw Exception("Diretório não encontrado!");
   }
-  //pegando os arquivos dentro do diretório
-  final arquivos = dir.listSync().where((element)=> element is File && element.path.endsWith('.csv'));
 
-  //inicializando lista para chamar instâncias da classe
-  List <Dados> registros = [];
+  final arquivos = dir.listSync().where((element) => element is File && element.path.endsWith('.csv'));
+  List<Dados> registros = [];
 
-  for(var file in arquivos){
+  for(var file in arquivos) {
     final arquivoNome = basename(file.path);
     final estado = arquivoNome.split('_')[0];
     final entrada = File(file.path).openRead();
-    final linhas = await entrada.transform(utf8.decoder).transform(CsvToListConverter()).toList();
+    final linhas = await entrada.transform(latin1.decoder).transform(CsvToListConverter()).toList();
 
-    if(linhas.isEmpty){
+    if(linhas.isEmpty) {
       throw Exception("Erro ao converter alguma das linhas!");
     }
 
-    for(int i=1; i<linhas.length; i++){
+    for(int i = 1; i < linhas.length; i++) {
       var linha = linhas[i];
 
-      try{
-        //armazenando aos valores em variáveis para jogar na lista e depois na classe
-        final dataHora = DateTime.parse(linha[0]);
-        final temperatura = double.parse(linha[1].toString());
-        final umidade = double.parse(linha[2].toString());
-        final velocidadeVento = double.parse(linha[3].toString());
-        final ventoDirecao = double.parse(linha[4].toString());
+      try {
+        // otendo as datas
+        final mes = linha[0] is String ? int.parse(linha[0]) : (linha[0] as num).toInt();
+        final dia = linha[1] is String ? int.parse(linha[1]) : (linha[1] as num).toInt();
+        final hora = linha[2] is String ? int.parse(linha[2]) : (linha[2] as num).toInt();
+
+        //armazenando as datas dentro de uma variável dateTime
+        final dataHora = DateTime(DateTime.now().year, mes, dia, hora);
+
+        // obtendo os valores baseado nos índices
+        final temperatura = linha[3] is String ? double.parse(linha[3]) : (linha[3] as num).toDouble();
+        final umidade = linha[4] is String ? double.parse(linha[4]) : (linha[4] as num).toDouble();
+        final velocidadeVento = linha[5] is String ? double.parse(linha[5]) : (linha[5] as num).toDouble();
+        final ventoDirecao = linha[6] is String ? double.parse(linha[6]) : (linha[6] as num).toDouble();
 
         registros.add(Dados(
-            estado: estado, dataHora: dataHora,
-            temperatura: temperatura, umidade: umidade,
-            velVento: velocidadeVento, dirVento: ventoDirecao));
+            estado: estado,
+            dataHora: dataHora,
+            temperatura: temperatura,
+            umidade: umidade,
+            velVento: velocidadeVento,
+            dirVento: ventoDirecao
+        ));
 
-      } catch (e){
+      } catch (e) {
         print("Erro ao processar a linha: $e");
+        print("Linha com problema: $linha");
       }
-
     }
-
-
   }
 
   return registros;
-
 }
