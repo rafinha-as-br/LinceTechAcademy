f78481e581330a9c11043be695a9e44ad7255d98
Rafael Antunes <rafinha72465@gmail.com>
2025-05-07 19:28:32 -0300

    apf1_funcoes - ap1.dart concluído!

--- formacaoFlutter/atividades_praticas/lib/apf1_funcoes/ap1.dart
+++ formacaoFlutter/atividades_praticas/lib/apf1_funcoes/ap1.dart
@@ -1,28 +1,63 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
+void main() {
+  runApp(myApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  
+class myApp extends StatefulWidget {
+  const myApp({super.key});
+
+  @override
+  State<myApp> createState() => _myAppState();
+}
+
+class _myAppState extends State<myApp> {
+  var color = Colors.black;
+  var random = Random();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child:  Text(
-            'Primeira atividade de Flutter!',
-            style: TextStyle(fontSize: 50),
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Center(
+              child: Text(
+                'Primeira atividade flutter!',
+                style: TextStyle(fontSize: 24, color: color),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  switch (random.nextInt(5)){
+                    case 1:
+                      color = Colors.yellow;
+                      break;
+                    case 2:
+                      color = Colors.blue;
+                      break;
+                    case 3:
+                      color = Colors.orange;
+                      break;
+                    case 4:
+                      color = Colors.red;
+                      break;
+                    case 5:
+                      color = Colors.green;
+                      break;
+
+                  }
+                });
+              },
+              child: Text("Mudar de cor!"),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
