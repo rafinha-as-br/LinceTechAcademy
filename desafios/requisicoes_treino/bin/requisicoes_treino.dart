2b3158ad86329c6b2c6fc9f318f32326ad596ec1
Rafael Antunes <rafinha72465@gmail.com>
2025-05-09 21:23:53 -0300

    Treinamento de requisições pronto

--- desafios/requisicoes_treino/bin/requisicoes_treino.dart
+++ desafios/requisicoes_treino/bin/requisicoes_treino.dart
@@ -1,4 +1,33 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert'; // Para trabalhar com JSON
 
-void main(List<String> arguments) {
+void main() async{
+  var url1 = Uri.parse('https://jsonplaceholder.typicode.com/posts/1');
+  var resposta = await http.get(url1);
+
+  if(resposta.statusCode==200){
+    var dados = jsonDecode(resposta.body);
+    print('Título: ${dados['title']}');
+  } else{
+    print("Erro ${resposta.statusCode}");
+  }
+
+  var url2 = Uri.parse("https://jsonplaceholder.typicode.com/posts");
+  var resposta2 = await http.post(
+    url2,
+    headers: {'Content-Type': 'application/json'},
+    body: jsonEncode({
+      'title': 'Post de Teste',
+      'body': 'Conteúdo qualquer',
+      'userId': 123,
+    }),
+  );
+
+  if (resposta.statusCode == 200) {
+    var dados = jsonDecode(resposta.body);
+    print('Post criado com ID: ${dados['id']}');
+  } else {
+    print('Erro: ${resposta.statusCode}');
+  }
 
 }
