f399497aeae48b3d951d5a5286f1a6049f271d14
Rafael Antunes <rafinha72465@gmail.com>
2025-05-15 14:21:10 -0300

    Atividade: apf2_funcoes - Conluída

--- formacaoFlutter/atividades_praticas/lib/apf2_funcoes/ap3.2.dart
+++ formacaoFlutter/atividades_praticas/lib/apf2_funcoes/ap3.2.dart
@@ -1,9 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(ap32App());
 }
-
 
 class ap32App extends StatefulWidget {
   const ap32App({super.key});
@@ -14,66 +15,119 @@ class ap32App extends StatefulWidget {
 
 enum Formato { quadrado, circulo }
 
+enum Cores { azul, branco, vermelho, verde, preto, amarelo }
 
 class _ap32AppState extends State<ap32App> {
-
+  //começa como quadrado por padrão
   String stringBotao = 'Mudar para círculo';
   Formato formatoAtual = Formato.quadrado;
 
-  void mudarFormaGeometrica(Formato formaAtual){
+  //começa como azul por padrão
+  Cores corAtual = Cores.azul;
+  Color cor = Colors.blue;
+
+
+  //funções para os botões
+  void mudarFormaGeometrica(Formato formaAtual) {
     setState(() {
-      if(formaAtual == Formato.quadrado){
+      if (formaAtual == Formato.quadrado) {
         formatoAtual = Formato.circulo;
         stringBotao = 'Mudar para quadrado';
-      } else{
+      } else {
         formatoAtual = Formato.quadrado;
         stringBotao = 'Mudar para círculo';
       }
     });
   }
 
+  Cores mudarCor() {
+    final random = Random();
+    final cores = Cores.values;
+    return cores[random.nextInt(cores.length)];
+  }
+
   @override
   Widget build(BuildContext context) {
+    //Switch case para definir cor
+    switch (corAtual) {
+      case Cores.azul:
+        cor = Colors.blue;
+        break;
+      case Cores.amarelo:
+        cor = Colors.yellow;
+        break;
+      case Cores.branco:
+        cor = Colors.white;
+        break;
+      case Cores.preto:
+        cor = Colors.black;
+        break;
+      case Cores.verde:
+        cor = Colors.green;
+        break;
+      case Cores.vermelho:
+        cor = Colors.red;
+        break;
+    }
 
     //switch case para construçao do formato geometrico
     Widget formaGeometrica;
-
     switch (formatoAtual) {
       case Formato.quadrado:
         formaGeometrica = Container(
           width: 100,
           height: 100,
-          color: Colors.blue,
+          color: cor,
         );
         break;
       case Formato.circulo:
         formaGeometrica = Container(
           width: 100,
           height: 100,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            shape: BoxShape.circle,
-          ),
+          decoration: BoxDecoration(color: cor, shape: BoxShape.circle),
         );
         break;
     }
 
-
-
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.grey,
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ElevatedButton(onPressed: () {
-                    mudarFormaGeometrica(formatoAtual);
-                  }, child: Text(stringBotao)),
-                  //ElevatedButton(onPressed: onPressed, child: child),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                    ),
+                    onPressed: () {
+                      mudarFormaGeometrica(formatoAtual);
+                    },
+                    child: Text(
+                      stringBotao,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(width: 20,),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                    ),
+                      onPressed: () {
+                    setState(() {
+                      corAtual = mudarCor();
+                    });
+                  },
+                      child: Text(
+                        'Mudar cor', style: TextStyle(color: Colors.white),
+                      )
+                  ),
                 ],
               ),
+              SizedBox(height: 10),
               formaGeometrica,
             ],
           ),
@@ -82,7 +136,3 @@ class _ap32AppState extends State<ap32App> {
     );
   }
 }
-
-
-
-
